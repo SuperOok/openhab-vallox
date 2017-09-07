fb28f2c8a25831b0da9a075897107f5634258bfb
gitMiguel <miikajukka@gmail.com>
2017-09-07 09:47:37 +0300

    Update namings
    Thing type should be named by the digit protocol which is common to many vallox air ventilation machines. After that configuring your thing id to example "kwl90se" or "110ser" channel naming would be much more logic. Like this: vallox:digit:kwl90se or vallox:digit:110ser.

--- src/org.eclipse.smarthome.binding.vallox/src/org/eclipse/smarthome/binding/vallox/service/ValloxBindingConstants.java
+++ src/org.eclipse.smarthome.binding.vallox/src/org/eclipse/smarthome/binding/vallox/service/ValloxBindingConstants.java
@@ -5,7 +5,7 @@ import org.eclipse.smarthome.core.thing.ThingTypeUID;
 public class ValloxBindingConstants {
 
     public static final String BINDING_ID = "vallox";
-    public static final ThingTypeUID THING_TYPE_VALLOX_SERIAL = new ThingTypeUID(BINDING_ID, "kwl90se");
+    public static final ThingTypeUID THING_TYPE_VALLOX_SERIAL = new ThingTypeUID(BINDING_ID, "digit");
 
     public static final String PARAMETER_HOST = "host";
     public static final String PARAMETER_PORT = "port";
